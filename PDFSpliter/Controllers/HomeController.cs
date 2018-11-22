107316045a0fda46a05fe86bca14ff9993b7aade
Treveshan Naidoo <treveshan.naidoo@chillisoft.co.za>
2018-11-22 14:13:25 +0200

    feat: WIP View PDF

---
+++ PDFSpliter/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PDFSpliter.Models;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PDFSpliter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ClaimDocumentModel claimDocumentModel)
+        {
+            var files = claimDocumentModel.files;
+            var fileExt = Path.GetExtension(files.FileName)?.ToUpper();
+
+            if (fileExt == ".PDF")
+            {
+                var documentName = claimDocumentModel.ClaimNumber + " " + claimDocumentModel.DocumentName + ".pdf";
+                var stream = files.InputStream;
+                var pdfDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+                var pageNumbers = claimDocumentModel.Pages.Split(new []{','},StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+                var pdfDocumentPages = pdfDocument.Pages;
+
+                var documentOfPageOne = new PdfDocument();
+                foreach (var pageNumber in pageNumbers)
+                {
+                    documentOfPageOne.AddPage(pdfDocumentPages[pageNumber]);
+                }
+
+                documentOfPageOne.Info.Author = "Chillisoft";
+             
+                var path = Server.MapPath("~/Uploads");
+                var combine = Path.Combine(path, documentName);
+                documentOfPageOne.Save(combine);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                ViewBag.FileStatus = "Invalid file format.";
+                return View();
+
+            }
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PDFSpliter/Models/ClaimDocumentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PDFSpliter.Models
+{
+    public class ClaimDocumentModel
+    {
+        [Required]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Select File")]
+        public HttpPostedFileBase files { get; set; }
+        public string ClaimNumber { get; set; }
+        public string DocumentName { get; set; }
+        public string Pages { get; set; }
+        //public IEnumerable<DocumentPage> DocumentPages { get; set; }
+    }
+
+    public class FileDetailsModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Uploaded File")]
+        public String FileName { get; set; }
+        public byte[] FileContent { get; set; }
+        
+    }
+
+    public class DocumentPage
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
