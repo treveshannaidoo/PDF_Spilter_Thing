fb1b2bacee66aa62bd87fc1b6ad0db5a466f30df
Treveshan Naidoo <treveshan.naidoo@chillisoft.co.za>
2018-11-22 14:49:51 +0200

    feat: WIP View pdf

--- PDFSpliter/App_Start/BundleConfig.cs
+++ PDFSpliter/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace PDFSpliter
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/pdfjs").Include(
+                      "~/Scripts/pdf.js", "~/Scripts/pdf.worker.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
